5f41fff45dd79d945ea3978c2cb7b88dac1b010e
Leonardo Ramos <leoramosdsousa@gmail.com>
2021-07-08 12:48:37 -0300

    installed nodemon and added methods http

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+const { response } = require('express');
 const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello world Ignite!' });
+app.get('/courses', (request, response) => {
+    return response.json(['course 1', 'course 2', 'course 3']);
+});
+
+app.post('/courses', (request, reponse) => {
+    return response.json(['course 1', 'course 2', 'course 3', 'course 4']);
+});
+
+app.put('/courses/:id', (request, response) => {
+    return response.json(['course 6', 'course 2', 'course 3', 'course 4']);
+});
+
+app.patch('/courses/:id', (request, reponse) => {
+    return response.json(['course 6', 'course 7', 'course 3', 'course 4']);
+});
+
+app.delete('/courses/:id', (request, reponse) => {
+    return reponse.json(['course 6', 'course 2', 'course 4']);
 });
 
 app.listen(3333);
